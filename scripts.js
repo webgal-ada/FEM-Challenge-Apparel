d03158d78d82fc5dc7e10f7f11c8619765aaa513
Aurora <ada10016@yahoo.com>
2020-08-08 22:14:56 -0400

    styled input btn, however inactive

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('submit')
+const email = document.getElementById('email')
+
+const ErrMessage = document.getElementById('emailErrorMsg')
+
+const alertIcon = document.getElementById('alertIcon')
+
+form.addEventListener('submit')
